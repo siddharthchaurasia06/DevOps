d9d0ee87e23c2477ac614e27e8136f03b346b033
Siddharth <sidharthchaurasia064@gmail.com>
2025-07-28 13:11:38 +0530

    some modificaions - 2

--- Terraform/varriables/parent/varriable.tf
+++ Terraform/varriables/parent/varriable.tf
@@ -1,2 +1,10 @@
-variable "x" {} 
-variable "y" {}
+variable "x" { 
+  description = "Name of the resource group"
+  type        = string
+  default     = "sid2-rg"
+} 
+variable "y" { 
+  description = "Location for the resource group"
+  type        = string
+  default     = "East US"
+}

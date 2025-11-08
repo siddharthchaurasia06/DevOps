987a177173bf4234fff9fa2c807f870a974b86ea
Siddharth <sidharthchaurasia064@gmail.com>
2025-11-09 05:08:09 +0530

    added some code

--- kubernete/Todo_app/MicroTodoUI/src/config.js
+++ kubernete/Todo_app/MicroTodoUI/src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 
 const config = {
-  GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://default-get-tasks-url',
-  DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://default-delete-task-url',
-  CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://default-create-task-url',
+  GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || '10.0.54.159',
+  DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || '10.0.50.26',
+  CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || '10.0.149.5',
 };
 
 export default config;

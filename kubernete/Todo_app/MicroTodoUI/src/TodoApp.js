41b112c264759c07b4dd5cff1e947e1edfff15ad
Siddharth <sidharthchaurasia064@gmail.com>
2025-11-26 12:51:52 +0530

    added some

--- kubernete/Todo_app/MicroTodoUI/src/TodoApp.js
+++ kubernete/Todo_app/MicroTodoUI/src/TodoApp.js
@@ -6,7 +6,7 @@ import { Box } from '@mui/material';
 import config from './config'; // Import the config file with environment variable support
 
 // Use the config to define API URLs
-const GET_TASKS_API_BASE_URL = "http://get-tasks-api.6weathers.com";
+const GET_TASKS_API_BASE_URL = "http://get-task-api.6weathers.com";
 const DELETE_TASK_API_BASE_URL = "http://delete-task-api.6weathers.com";
 const CREATE_TASK_API_BASE_URL = "http://add-task-api.6weathers.com";
 
